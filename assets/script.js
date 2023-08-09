a1e5fd975252d3bb17f2c329f6228ccb3f49302a
Jacob Campa <jacob.campa@outlook.com>
2023-08-08 21:27:27 -0500

    added image populate in js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var searchedWord = document.getElementById("search")
 var submit = document.querySelector("#submit")
+var cards = document.querySelector("#card-display")
 
 function callScryfall(event) {
     event.preventDefault()
@@ -8,13 +9,18 @@ function callScryfall(event) {
     var reqApi = `https://api.scryfall.com/cards/search?q=${userInput}`
 
     fetch(reqApi)
-    .then((response) => {
-        console.log(response)
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data)
-    })
-}
+        .then((response) => {
+            console.log(response)
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            var cardsFound = []
+                var cardIMG = document.createElement("img")
+                cardIMG.src = (data.data[0].image_uris.png)
+                cards.appendChild(cardIMG)
+            })
+
+        };
 
 submit.addEventListener("click", callScryfall)
